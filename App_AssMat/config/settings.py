a9127fe8dc176ca14d0ac23a2ad8f7debb659ac6
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-11 18:26:40 +0100

    Maj account models.py and setting.py for media

--- App_AssMat/config/settings.py
+++ App_AssMat/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- App_AssMat/dashboard/urls.py
+++ App_AssMat/dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from dashboard.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', index)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
