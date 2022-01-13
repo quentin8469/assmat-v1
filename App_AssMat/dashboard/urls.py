6e544e9c8a07fccf54c649594ff4e70b5ca32fa8
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-13 15:43:36 +0100

    ad include html for liste_famille

--- App_AssMat/dashboard/urls.py
+++ App_AssMat/dashboard/urls.py
@@ -3,6 +3,9 @@ from dashboard.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+app_name = 'dashboard'
+
 urlpatterns = [
     path('dashboard/', index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
