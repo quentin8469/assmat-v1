eec951ca3f42be414a48d6fdb340fa66a710d1ad
Quentin Laparra <quentin.laparra@gmail.com>
2022-01-24 23:33:40 +0100

    WIP contrat

--- App_AssMat/contrat/urls.py
+++ App_AssMat/contrat/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 
 from contrat.views import (ContratView,
                            ContratCreate,
+                           EnfantView,
+                           RemunerationView,
+                           CongePayeView,
                            
                            )
 
@@ -14,6 +17,9 @@ app_name = "contrat"
 urlpatterns = [
     path('', ContratView.as_view() , name='liste_contrats'),
     path('creation_contrat/', ContratCreate.as_view() , name='creation_contrats'),
+    path('liste_enfants/', EnfantView.as_view() , name='liste_enfants'),
+    path('liste_remunerations/', RemunerationView.as_view(), name='liste_remunerations'),
+    path('liste_congepayer/', CongePayeView.as_view() , name='liste_congepayer'),
     
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- App_AssMat/contrat/views.py
+++ App_AssMat/contrat/views.py
@@ -1,19 +1,37 @@
 
 from django.views.generic import ListView, CreateView
 
-from contrat.models import Contrat
+from contrat.models import Contrat, Enfant, Remuneration, CongePaye
 from contrat.forms import NewContratForm
 
-# Contrat CRUD
+# Liste
 
 class ContratView(ListView):
     model = Contrat
     context_object_name = 'contrats'
     template_name = 'contrat/contrat_list.html'
+
+
+class EnfantView(ListView):
+    model = Enfant
+    context_object_name = 'enfants'
+    template_name = 'contrat/enfant_list.html'
+
+
+class RemunerationView(ListView):
+    model = Remuneration
+    context_object_name = 'remunerations'
+    template_name = 'contrat/remuneration_list.html'
+
     
+class CongePayeView(ListView):
+    model = CongePaye
+    context_object_name = 'conges'
+    template_name = 'contrat/conge_list.html'
+    
+# Create   
 class ContratCreate(CreateView):
     model = Contrat
     form_class = NewContratForm
     template_name = 'contrat/contrat_create.html'
     success_url = '/dashboard/'
-
